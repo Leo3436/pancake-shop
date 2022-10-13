d4336268662037fcf97e6dc0e6f861e1aca5fa09
LeoLeo <38936522+Leo3436@users.noreply.github.com>
2022-10-13 10:13:45 +0200

    Initial commit

---
+++ src/main/java/com/example/pancakeshop/PancakeShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.pancakeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PancakeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PancakeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pancakeshop/PancakeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pancakeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PancakeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
